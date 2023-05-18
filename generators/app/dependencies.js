eb0790037e8d945557decdfefe8cadef99f7600a
James Pyne <james@pala.london>
2023-05-18 15:25:10 +0100

    Update deps and Node version

--- generators/app/dependencies.js
+++ generators/app/dependencies.js
@@ -4,13 +4,13 @@ exports.PRETTIER_STANDARD_DEVDEPS = {
 
 exports.ESLINT_STANDARD_DEVDEPS = {
   eslint: "^8",
-  "eslint-config-airbnb-typescript": "^16",
+  "eslint-config-airbnb-typescript": "^17",
   "eslint-config-airbnb": "^19",
   "eslint-config-prettier": "^8",
   "eslint-import-resolver-babel-module": "^5",
   "eslint-plugin-import": "^2",
   "eslint-plugin-jsx-a11y": "^6",
-  "eslint-plugin-prettier": "^3",
+  "eslint-plugin-prettier": "^4",
   "eslint-plugin-react": "^7",
   "eslint-plugin-react-hooks": "^4",
   "@typescript-eslint/parser": "^5",
@@ -23,9 +23,9 @@ exports.LINTSTAGED_STANDARD_DEVDEPS = {
 };
 
 exports.GRAPHQL_CODEGEN_STANDARD_DEVDEPS = {
-  "@graphql-codegen/cli": "^2",
-  "@graphql-codegen/typescript": "^2",
-  "@graphql-codegen/typescript-operations": "^2",
+  "@graphql-codegen/cli": "^3",
+  "@graphql-codegen/typescript": "^3",
+  "@graphql-codegen/typescript-operations": "^3",
   "@graphql-codegen/typescript-react-apollo": "^3"
 }
 
@@ -34,5 +34,5 @@ exports.GRAPHQL_CODEGEN_STANDARD_DEPS = {
 }
 
 exports.SVGR_STANDARD_DEVDEPS = {
-  "@svgr/cli": "^6"
+  "@svgr/cli": "^8"
 }
